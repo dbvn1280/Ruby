f5a92dac3124e95f74b013783b54937eba5d358b
Hieu <dbvn1280@gmail.com>
2019-04-15 11:03:30 -0400

    first github

---
+++ tesst.rb
@@ -0,0 +1,25 @@
+s = String.new('Deabak ')
+puts s
+
+puts "Have a nice  \"my name's\" #{s}"
+
+a = %-12/04/2019 -
+
+puts a.class
+
+puts a.empty?
+
+s << a
+
+puts s
+
+b = 'Hello \n'
+puts b.chomp
+
+string = <<N
+1
+2
+3
+N
+
+puts string
